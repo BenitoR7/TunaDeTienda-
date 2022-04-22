1b119040de36fa41877209431ad0d8d690498907
Ben <benitorodriguez3000@outlook.com>
2022-04-22 12:57:19 -0500

    Firebase Mapped. Still need Order info added.

--- TiendaDeTuna/TiendaDeTuna/ViewModels/FirebaseViewModel.swift
+++ TiendaDeTuna/TiendaDeTuna/ViewModels/FirebaseViewModel.swift
@@ -1,34 +1,34 @@
 import Foundation
 import FirebaseFirestore
 class FirebaseViewModel: ObservableObject {
-@Published var checkOutFields = [CheckOutFields]()
-private var  db = Firestore.firestore()
+    @Published var checkOutFields = [CheckOutFields]()
+    private var  db = Firestore.firestore()
     
-func getAllData()
-    { db.collection("checkOutFields").addSnapshotListener { (querySnapshot, error) in
-              guard let documents = querySnapshot?.documents else {
-                  print("No documents")
-                  return }
-
-              self.checkOutFields = documents.map { (queryDocumentSnapshot) -> CheckOutFields in
-                  let data = queryDocumentSnapshot.data()
-                  let name = data["name"] as? String ?? ""
-                  let phoneNum = data["phoneNum"] as? String ?? ""
-
-                  return CheckOutFields(name: name, phoneNum: phoneNum)}}}
-
-func addNewData(checkOutField: String)
-    {do {_ = try db.collection("checkOutFields").addDocument(data: ["name": checkOutField, "phoneNum":checkOutField])  }
-             catch {print(error.localizedDescription) }}
+    func getAllData() {
+        db.collection("checkOutFields").addSnapshotListener { (querySnapshot, error) in
+            guard let documents = querySnapshot?.documents else {
+                print("No documents")
+                return }
+            
+            self.checkOutFields = documents.map { (queryDocumentSnapshot) -> CheckOutFields in
+                let data = queryDocumentSnapshot.data()
+                let name = data["name"] as? String ?? ""
+                let phoneNum = data["phoneNum"] as? String ?? ""
+                
+                return CheckOutFields(name: name, phoneNum: phoneNum)}}
+    }
     
+    func addNewData(name: String, phoneNum: String) {
+        db.collection("checkOutFields").addDocument(data: ["name": name, "phoneNum": phoneNum])
+    }
 }
 
 import Foundation
 import FirebaseFirestore
 struct CheckOutFields: Codable, Identifiable {
-
-  var id: String = UUID().uuidString
-var name: String?
+    
+    var id: String = UUID().uuidString
+    var name: String?
     var phoneNum: String?
-
+    
 }
